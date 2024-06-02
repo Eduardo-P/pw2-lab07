4e3c0b551dd817ff4db635fccdb60fffaf001d12
Eduardo <eportugalpor@unsa.edu.pe>
2024-06-02 00:21:01 -0500

    Creando el Modelo 'Destination' con atributos base, creadno objeto Destination en la vista para luego ser usado en el templates

--- lab07/travello/models.py
+++ lab07/travello/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Destination:
+    nombreCiudad: str
+    descripcionCiudad: str
+    imagenCiudad: str
+    precioTour: int
+    ofertaTour: bool
