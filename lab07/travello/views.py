d04cdefeb8b69e6e48522933ab3aed95388e4b31
Eduardo <eportugalpor@unsa.edu.pe>
2024-06-02 00:44:22 -0500

    Creando dos objetos mas de tipo Destination en la vista, para luego usar un tag for en el template colocando los datos de los 3 destinos en el html

--- lab07/travello/views.py
+++ lab07/travello/views.py
@@ -6,6 +6,21 @@ def index(request):
     dest1 = Destination()
     dest1.nombreCiudad = 'Mumbai'
     dest1.descripcionCiudad = 'The City That Never Sleeps'
+    dest1.imagenCiudad = 'destination_1.jpg'
     dest1.precioTour = 700
     
-    return render(request, "index.html", {'dest1': dest1})
+    dest2 = Destination()
+    dest2.nombreCiudad = 'Hyderabad'
+    dest2.descripcionCiudad = 'First Biryani, The Sherwani'
+    dest2.imagenCiudad = 'destination_2.jpg'
+    dest2.precioTour = 650
+    
+    dest3 = Destination()
+    dest3.nombreCiudad = 'Bengaluru'
+    dest3.descripcionCiudad = 'Beatutiful City'
+    dest3.imagenCiudad = 'destination_3.jpg'
+    dest3.precioTour = 750
+    
+    dests = {dest1, dest2, dest3}
+    
+    return render(request, "index.html", {'dests': dests})
