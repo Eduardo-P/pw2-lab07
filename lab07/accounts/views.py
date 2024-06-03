06034502f8996c6f1655bc124daa60d6b70a40e6
Eduardo <eportugalpor@unsa.edu.pe>
2024-06-03 10:12:56 -0500

    Agregando en la vista 'register' mensajes informativos cuando se presenta algun error durante el registro del usuario, ademas de agregar esta seccion para mostrar los mensajes en el template 'register.html'

--- lab07/accounts/views.py
+++ lab07/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
@@ -13,15 +14,18 @@ def register(request):
         
         if password1 == password2:
             if User.objects.filter(username=username).exists():
-                print('Username taken')
+                messages.info(request, 'Username Taken')
+                return redirect('register')
             elif User.objects.filter(email=email).exists():
-                print('email taken')
+                messages.info(request, 'Email taken')
+                return redirect('register')
             else:
                 user = User.objerts.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
                 print('user created')
         else:
-            print('password not matching..')
+            messages.info(request, 'password not matching..')
+            return redirect('register')
         return redirect('/')
     
     else:
