7b9fa7fb69973a6cbf57f9479c1659c7c9eb2d15
Eduardo <eportugalpor@unsa.edu.pe>
2024-06-03 00:37:43 -0500

    Agregando lugares turisticos de Arequipa a la base de datos. Modificando la vista para obtener todos los objetos 'Destination' y mostrarlos a traves del template

--- lab07/travello/views.py
+++ lab07/travello/views.py
@@ -3,27 +3,7 @@ from .models import Destination
 
 # Create your views here.
 def index(request):
-    dest1 = Destination()
-    dest1.nombreCiudad = 'Mumbai'
-    dest1.descripcionCiudad = 'The City That Never Sleeps'
-    dest1.imagenCiudad = 'destination_1.jpg'
-    dest1.precioTour = 700
-    dest1.ofertaTour = False
     
-    dest2 = Destination()
-    dest2.nombreCiudad = 'Hyderabad'
-    dest2.descripcionCiudad = 'First Biryani, The Sherwani'
-    dest2.imagenCiudad = 'destination_2.jpg'
-    dest2.precioTour = 650
-    dest2.ofertaTour = True
-    
-    dest3 = Destination()
-    dest3.nombreCiudad = 'Bengaluru'
-    dest3.descripcionCiudad = 'Beatutiful City'
-    dest3.imagenCiudad = 'destination_3.jpg'
-    dest3.precioTour = 750
-    dest3.ofertaTour = True
-    
-    dests = {dest1, dest2, dest3}
+    dests = Destination.objects.all()
     
     return render(request, "index.html", {'dests': dests})
