a945ae82308e0e09cf598fd1560d07a0858154ad
Eduardo <eportugalpor@unsa.edu.pe>
2024-06-04 17:43:45 -0500

    Agregando la nueva vista 'destinationCreate' que esta encargada de mostrar el formulario para agregar nuevos destinos turisticos

--- lab07/travello/views.py
+++ lab07/travello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Destination
+from .forms import DestinationForm
 
 # Create your views here.
 def index(request):
@@ -7,3 +8,14 @@ def index(request):
     dests = Destination.objects.all()
     
     return render(request, "index.html", {'dests': dests})
+
+def destinationCreate(request):
+    form = DestinationForm(request.POST or None)
+    if form.id_valid():
+        form.save()
+        form = DestinationForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'destinationCreate.html', context)
