24d1d623190c376113323c94f2302f838a1819a9
Eduardo <eportugalpor@unsa.edu.pe>
2024-06-04 22:29:59 -0500

    Agregando ruta a urls.py para que la vista destinationList sea accesible

--- lab07/travello/urls.py
+++ lab07/travello/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('agregar_destino/', views.destinationCreate, name="DestinationCreate"),
     path('agregar_destino/<int:myID>/', views.destinationEdit, name='DestinationEdit'),
-    path('eliminar_destino/<int:myID>/', views.destinationDelete, name='DestinationDelete')
+    path('eliminar_destino/<int:myID>/', views.destinationDelete, name='DestinationDelete'),
+    path('lista_destinos/', views.destinationList, name='DestinationList')
 ]
